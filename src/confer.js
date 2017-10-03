4b823a904ec42017a79a84059fcd3b23d51684d8
eduardoanaya <gran_ed@hotmail.com>
2017-10-03 14:57:50 +0200

    Added deep equality check

--- src/confer.js
+++ src/confer.js
@@ -3,7 +3,7 @@ var R = require('rambda');
 module.exports = (collection, list) => {
   var results = collection;
   var filter = (val, idx) => {
-    results = R.filter(ce => ce[idx] === val, results);
+    results = R.filter(ce => R.equals(ce[idx], val), results);
   };
   var indexedMapper = R.addIndex(R.map);
 
--- test/confer.test.js
+++ test/confer.test.js
@@ -4,19 +4,19 @@ const confer = require('../src/confer');
 describe('Confer', () => {
   context('when the given colleciton list is an object', () => {
     const objectCollection = {
-      k1: ['elem1', 'elem3', 'elem2'],
-      k2: ['elem1', 2, 'elem3', 'elem4'],
+      k1: [{ deep: { deeper: 'elem1' } }, 'elem3', 'elem2'],
+      k2: [{ deep: { deeper: 'elem1' } }, 2, 'elem3', 'elem4'],
       k3: ['elem2', 'elem1', 'elem3'],
-      k4: ['elem1', 2],
+      k4: [{ deep: { deeper: 'elem1' } }, 2],
       k5: ['elem3', 'elem2', 'elem7'],
     };
 
     context('and it finds matches', () => {
-      const list = ['elem1', 2];
+      const list = [{ deep: { deeper: 'elem1' } }, 2];
       it('should return matched results', () => {
         expect(confer(objectCollection, list), 'to exhaustively satisfy', {
-          k2: ['elem1', 2, 'elem3', 'elem4'],
-          k4: ['elem1', 2],
+          k2: [{ deep: { deeper: 'elem1' } }, 2, 'elem3', 'elem4'],
+          k4: [{ deep: { deeper: 'elem1' } }, 2],
         });
       });
     });
